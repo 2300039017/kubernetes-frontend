336afecdcbad98ae2873543ac2615faf151335bf
2300039017 <2300039017@kluniversity.in>
2025-10-25 17:07:54 +0530

    ffg

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ import Computers from './components/Computers';
 import Mobiles from './components/Mobiles';
 import Laptops from './components/Laptops';
 import Pendrives from './components/Pendrives';
-import Home from './components/Home'
-import ProductPage from './components/ProductPage'
+import Home from './components/Home';
+import ProductPage from './components/ProductPage';
 import Cart from './components/Cart';
 import Payment from './components/Payment';
 import Orders from './components/Orders';
@@ -22,7 +22,6 @@ function App() {
   
   return (
     <>
-    <BrowserRouter basename="/ecommerce">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
@@ -40,7 +39,7 @@ function App() {
           <Route path="orders" element={<Orders />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+  
     </>
   )
 }

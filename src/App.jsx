40c2a196b3ed457a009b62d3885d93e6598f7f3f
BOPPE PALLI SHAHABAJ <2300039017@kluniversity.in>
2025-10-25 16:58:09 +0530

    Remove BrowserRouter from App component
    Removed BrowserRouter from the component structure.

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <BrowserRouter basename="/ecommerce">
+    
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
@@ -40,7 +40,7 @@ function App() {
           <Route path="orders" element={<Orders />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    
     </>
   )
 }
